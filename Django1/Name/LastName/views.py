11f4d66b3443a94206344c7b008ade7e412b47f5
Hetal Joshi <hetaljoshi787@gmail.com>
2024-01-20 19:44:02 +0530

    Static Files

--- Django1/Name/LastName/views.py
+++ Django1/Name/LastName/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def lastname(request):
-    return render(request,'lastname.html')
+    return render(request,'LastName/lastname.html')
 
 
